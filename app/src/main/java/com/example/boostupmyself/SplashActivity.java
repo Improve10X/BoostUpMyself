ddb62f5dd5f6005313396ec9f5a84ebcf7c42697
Suresh <suresh.sanganaboyana@improve10x.com>
2022-12-29 16:12:44 +0530

    Created SplashActivity

---
+++ app/src/main/java/com/example/boostupmyself/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.boostupmyself;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this, HomeScreenActivity.class);
+            startActivity(intent);
+            finish();
+        },3000);
+    }
+}
