f1f90c219616e7537f484770fee10d1213a02e35
Suresh <suresh.sanganaboyana@improve10x.com>
2022-12-29 23:59:47 +0530

    created RecyclerView for CategoriesActivity

--- app/src/main/java/com/example/boostupmyself/CategoriesActivity.java
+++ app/src/main/java/com/example/boostupmyself/CategoriesActivity.java
@@ -1,15 +1,31 @@
 package com.example.boostupmyself;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.example.boostupmyself.databinding.ActivityCategoriesBinding;
+
+import java.util.ArrayList;
 
 public class CategoriesActivity extends AppCompatActivity {
+
+    private ArrayList<Category> categories;
+    ActivityCategoriesBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_categories);
+        binding = ActivityCategoriesBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().setTitle("Categories");
+        setupCategoriesRv();
+    }
+
+    private void setupCategoriesRv() {
+        binding.categoriesRv.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/boostupmyself/Category.java
@@ -0,0 +1,4 @@
+package com.example.boostupmyself;
+
+public class Category {
+}
