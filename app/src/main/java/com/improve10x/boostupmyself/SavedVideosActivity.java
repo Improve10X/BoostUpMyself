c8468cae9212931ced641ae8a42db0dc95a93d75
Suresh <suresh.sanganaboyana@improve10x.com>
2023-01-02 23:01:34 +0530

    SavedVideosActivity created

---
+++ app/src/main/java/com/improve10x/boostupmyself/SavedVideosActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.boostupmyself;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SavedVideosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_videos);
+    }
+}
