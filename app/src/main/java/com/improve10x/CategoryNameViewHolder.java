6a99f578d38592722a338c4f6840573ffc22723c
roopa <roopa.peddinti@improve10x.com>
2023-01-02 12:31:47 +0530

    created recycler view for category name activity

---
+++ app/src/main/java/com/improve10x/CategoryNameViewHolder.java
@@ -0,0 +1,19 @@
+package com.improve10x;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.improve10x.boostupmyself.databinding.ActivityCategoryNameBinding;
+import com.improve10x.boostupmyself.databinding.CategoryNameItemBinding;
+
+public class CategoryNameViewHolder extends RecyclerView.ViewHolder {
+
+    CategoryNameItemBinding binding;
+
+    public CategoryNameViewHolder(CategoryNameItemBinding categoryNameItemBinding) {
+        super(categoryNameItemBinding.getRoot());
+        this.binding = categoryNameItemBinding;
+    }
+}
